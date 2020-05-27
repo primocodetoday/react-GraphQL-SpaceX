b6bd44e23e0bab724053566f8d69eda5cfc92d76
primocodetoday <primocodetoday@gmail.com>
2020-05-27 23:31:35 +0200

    Provider Applied

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import MissionsContainer from './containers/MissionsContainer';
 import './App.css';
 
-function App() {
+const App = () => {
+  const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql',
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <main>
+        <MissionsContainer />
+      </main>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
---
+++ src/containers/MissionsContainer.js
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+
+const MissionsContainer = () => <p>Hello</p>;
+
+export default MissionsContainer;
